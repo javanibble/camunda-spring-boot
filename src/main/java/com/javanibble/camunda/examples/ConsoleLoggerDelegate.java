275bcc7aa912806534a23c355761be4abb1ccefb
Andre Mare <andre.mare@gmail.com>
2021-08-30 09:33:48 +0200

    Camunda Spring Boot Example

---
+++ src/main/java/com/javanibble/camunda/examples/ConsoleLoggerDelegate.java
@@ -0,0 +1,19 @@
+package com.javanibble.camunda.examples;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+
+@Component("logger")
+public class ConsoleLoggerDelegate implements JavaDelegate {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(ConsoleLoggerDelegate.class.getName());
+
+    public void execute(DelegateExecution execution) throws Exception {
+        LOGGER.info("Personal Message: " + execution.getVariable("personal-message"));
+    }
+
+}
